e9d0388477e8c97bf0e2dc96673ac7b39196b58c
Julian Lannigan <julian.lannigan@gmail.com>
2023-12-15 17:48:34 -0500

    chore: improve tests

--- src/tests/simplify.test.ts
+++ src/tests/simplify.test.ts
@@ -1,15 +1,17 @@
 import { simplify } from "../index"
 
-describe("interview", () => {
-    it("should do the simplification", () => {
-        expect(simplify("cb+cba")).toEqual("bc+abc");
-        expect(simplify("2xy-yx")).toEqual("xy");
-        expect(simplify("-a+5ab+3a-c-2a")).toEqual("-c+5ab");
-        expect(simplify("-abc+3a+2ac")).toEqual("3a+2ac-abc");
-        expect(simplify("xyz-xz")).toEqual("-xz+xyz");
-        expect(simplify("a+ca-ab")).toEqual("a-ab+ac");
-        expect(simplify("xzy+zby")).toEqual("byz+xyz");
-        expect(simplify("-y+x")).toEqual("x-y");
-        expect(simplify("y-x")).toEqual("-x+y");
-    })
+describe('simplify()', () => {
+  it.each([
+    ['cb+cba', 'bc+abc'],
+    ['2xy-yx', 'xy'],
+    ['-a+5ab+3a-c-2a', '-c+5ab'],
+    ['-abc+3a+2ac', '3a+2ac-abc'],
+    ['xyz-xz', '-xz+xyz'],
+    ['a+ca-ab', 'a-ab+ac'],
+    ['xzy+zby', 'byz+xyz'],
+    ['-y+x', 'x-y'],
+    ['y-x', '-x+y'],
+  ])('given "%s" it should return "%s"', (given, expected) => {
+    expect(simplify(given)).toEqual(expected)
+  });
 })
